3284bf92e3961a1066aa9f50299fd14d6b081763
justmfundo-github <justmfundo@gmail.com>
2023-10-26 11:53:40 +0200

    Base React app created. Main components separated in main.js Simple html/jsx frame added, nothing dynamic

---
+++ complex-react-app/app/Main.js
@@ -0,0 +1,22 @@
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+      <Footer />
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
+
+if (module.hot) {
+  module.hot.accept();
+}
