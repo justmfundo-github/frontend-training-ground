95854861abd3f55b7b1af9773e238c25896909ea
justmfundo-github <justmfundo@gmail.com>
2023-11-28 16:20:08 +0200

    Context and Reducer changes

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,16 +1,60 @@
-import React from "react";
-import * as ReactDOM from "react-dom/client";
+import React, { useState, useReducer } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
+import Home from "./components/Home";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
+import CreatePost from "./components/CreatePost";
+import ViewSinglePost from "./components/ViewSinglePost";
+import Axios from "axios";
+import FlashMessages from "./components/FlashMessages";
+import ExampleContext from "./ExampleContext";
+
+Axios.defaults.baseURL = "http://localhost:8080";
 
 function Main() {
+  const initialState = {
+    loggedIn: Boolean(localStorage.getItem("complexappToken")),
+    flashMessages: [],
+  };
+  function ourReducer(state, action) {
+    switch (action.type) {
+      case "login":
+        return { loggedIn: true, flashMessages: state.flashMessages };
+      case "logout":
+        return { loggedIn: false, flashMessages: state.flashMessages };
+      case "flashMessage":
+        return { loggedIn: state.loggedIn, flashMessages: state.flashMessages.concat(action.value) };
+    }
+  }
+  const [state, dispatch] = useReducer(ourReducer, initialState);
+
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
+  const [flashMessages, setFlashMessages] = useState([]);
+
+  function addFlashMessage(msg) {
+    setFlashMessages((prev) => prev.concat(msg));
+  }
+
   return (
-    <>
-      <Header />
-      <HomeGuest />
-      <Footer />
-    </>
+    <ExampleContext.Provider value={{ addFlashMessage, setLoggedIn }}>
+      <BrowserRouter>
+        <FlashMessages messages={flashMessages} />
+        <Header loggedIn={loggedIn} />
+        <Routes>
+          <Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+          <Route path="/post/:id" element={<ViewSinglePost />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/about-us" element={<About />} />
+          <Route path="/terms" element={<Terms />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </ExampleContext.Provider>
   );
 }
 
