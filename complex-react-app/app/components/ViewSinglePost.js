8a746027ab4d9e487b41fd90880ff5e82032d162
justmfundo-github <justmfundo@gmail.com>
2024-01-18 14:37:19 +0200

    Making changes to data using EditPost

--- complex-react-app/app/components/ViewSinglePost.js
+++ complex-react-app/app/components/ViewSinglePost.js
@@ -3,6 +3,8 @@ import Page from "./Page";
 import { useParams, Link } from "react-router-dom";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
+import ReactMarkdown from "react-markdown";
+import { Tooltip as ReactToolTip } from "react-tooltip";
 
 function ViewSinglePost() {
   const { id } = useParams();
@@ -42,12 +44,14 @@ function ViewSinglePost() {
           <div className="d-flex justify-content-between">
             <h2>{post.title}</h2>
             <span className="pt-2">
-              <a href="#" className="text-primary mr-2" title="Edit">
+              <Link to={`/post/${post._id}/edit`} data-tooltip-content="Edit" data-tooltip-id="edit" className="text-primary mr-2">
                 <i className="fas fa-edit"></i>
-              </a>
-              <a className="delete-post-button text-danger" title="Delete">
+              </Link>
+              <ReactToolTip id="edit" className="custom-tooltip" />{" "}
+              <a data-tooltip-content="Delete" data-tooltip-id="delete" className="delete-post-button text-danger">
                 <i className="fas fa-trash"></i>
               </a>
+              <ReactToolTip id="delete" className="custom-tooltip" />
             </span>
           </div>
 
@@ -58,7 +62,9 @@ function ViewSinglePost() {
             Posted by <Link to={`/profile/${post.author.username}`}>{post.author.username}</Link> on {dateFormatted}
           </p>
 
-          <div className="body-content">{post.body}</div>
+          <div className="body-content">
+            <ReactMarkdown children={post.body} allowedElements={["p", "br", "strong", "em", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "li"]} />
+          </div>
         </div>
       </Page>
     );
